97210c77b2cdbdda77736d98668a4ae85324e7df
Kenneth Ormandy <kenneth@chloi.io>
2016-09-27 14:45:20 -0700

    Adds basic font loading

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import 'es6-shim'
 import app from 'ampersand-app'
-// import { loadCSS } from 'fg-loadcss'
+import { loadCSS } from 'fg-loadcss'
 import Router from './router'
 
 // CSS
-// require('./css/fonts-critical.css')
+// require('./css/fonts.scss')
 require('./css/index.scss')
 
 window.app = app.extend({
   init () {
     this.router = new Router()
     this.router.history.start()
-    // loadCSS('/css/fonts.css')
+    loadCSS('/css/fonts.css')
   }
 })
 
