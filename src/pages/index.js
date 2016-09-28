34890397d0e4b2c03bab407969c51b70a4286946
Kenneth Ormandy <kenneth@chloi.io>
2016-09-27 19:15:18 -0700

    Adds toggle able overlay sidebar that works on large and small screen sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { StickyContainer, Sticky } from 'react-sticky';
-import _ from 'lodash'
+import throttle from 'lodash.throttle'
 
 import SpecimenSawarabi from '../specimens/Sawarabi'
+import SpecimenMPlus from '../specimens/MPlus'
+import SpecimenKokoro from '../specimens/Kokoro'
 
 class StickySidebar extends React.Component {
   constructor () {
@@ -30,12 +32,16 @@ class StickySidebar extends React.Component {
         <div className="flex flex-wrap justify-end relative">
           <div className="col-12 md-col-9">{ self.props.children }</div>
           <div
-            className="col-9 bg-gray md-col-3 absolute md-relative top-0 right-0 height-100 md-height-auto temp-translate-sidebar"
+            className="col-12 md-col-3 absolute md-relative top-0 right-0 height-100 md-height-auto"
             style={{
+              // outline: '5px solid yellow'
               // TODO
               // translate: 'transformX(' + 0 + ')'
-            }}
-            onClick={ self.onClick }>
+            }}>
+            <div className="js-sidebar col-9 md-col-12 bg-gray right height-100 border absolute md-relaitve temp-translate-sidebar"
+                 style={{
+                   right: (self.props.show ? '0' : '-50') + '%'
+                 }}>
             <Sticky style={{ zIndex: 3, height: 0 }}>
               <div className="right col-9 md-col-12">
                 <div className="p2 bg-silver">
@@ -44,6 +50,7 @@ class StickySidebar extends React.Component {
                 </div>
               </div>
             </Sticky>
+            </div>
           </div>
         </div>
       </StickyContainer>
@@ -52,15 +59,79 @@ class StickySidebar extends React.Component {
 }
 
 StickySidebar.defaultProps = {
-
+  show: false // Default to false for pre-render
 }
 
 class Index extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      sidebar: false, // Default to false for mobile-first prerender
+      viewport: {
+        width: 1024, // TODO Change
+        height: 768
+      }
+    }
+
+    // If window is less than medium breakpoint, this.setState({ sidebar: true })
+
+    this.onClickSpecimen = this.onClickSpecimen.bind(this)
+
+    this.getViewportWidth = () => {
+      if (window && document) {
+        // Via http://stackoverflow.com/a/11744120/864799
+        return {
+          width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+          height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+        }
+      }
+    }
+
+    this.setViewportWidth = () => {
+      var viewport = this.getViewportWidth()
+      this.setState({
+        viewport: viewport,
+        sidebar: (viewport.width > this.props.sidebarMaxWidth) ? true : false
+      })
+    }
+
+    this.handleResize = () => {
+      return throttle(this.setViewportWidth, 500)
+    }
+
+  }
+
+  componentDidMount () {
+    const self = this
+    self.setViewportWidth()
+    window.addEventListener('resize', self.handleResize(), false)
+  }
+
+  componentWillUnmount () {
+    const self = this
+    window.removeEventListener('resize', self.handleResize(), false)
+  }
+
+  onClickSpecimen (e, f) {
+    const self = this
+    var el = e.target || false
+
+    // TODO Do this a better way, maybe with refs?
+    // TODO more possible contextual touch targets
+    console.log(self.state.viewport.width, self.props.sidebarMaxWidth)
+    if (self.state.viewport.width < self.props.sidebarMaxWidth) {
+      if ((self.state.sidebar === true) || (el && el.classList.contains('js-sidebar'))) {
+        self.setState({
+          sidebar: !self.state.sidebar
+        })
+      }
+    }
+  }
+
   render () {
     const self = this
     const data = self.props.data
-
-    console.log(self.props)
 
     // var specimenList = ['sawarabi', 'mplus']
     //
@@ -84,26 +155,17 @@ class Index extends React.Component {
     // })
 
     return (
-      <div style={{ height: 100 + '%' }}>
+      <div style={{ height: 100 + '%' }} onClick={ this.onClickSpecimen }>
         <h1>{ data.title }</h1>
         <div className="bg-red" style={{ height: 500 + 'px' }}>Header</div>
-        <StickySidebar { ...self.props } font="sawarabi">
+        <StickySidebar { ...self.props } font="sawarabi" show={ self.state.sidebar }>
           { React.createElement(SpecimenSawarabi) }
         </StickySidebar>
-        <StickySidebar { ...self.props } font="mplus">
-          <div style={{ height: 500 + 'px' }} className="bg-aqua">
-            <h2 className="mt0">M+</h2>
-          </div>
+        <StickySidebar { ...self.props } font="mplus" show={ self.state.sidebar }>
+          <SpecimenMPlus { ...self.props } />
         </StickySidebar>
-        <StickySidebar { ...self.props } font="kokoro">
-          <div style={{ height: 500 + 'px' }} className="bg-olive">
-            <h2 className="mt0">Kokoro</h2>
-          </div>
-        </StickySidebar>
-        <StickySidebar { ...self.props } font="nicomojiplus">
-          <div style={{ height: 500 + 'px' }} className="bg-yellow">
-            <h2 className="mt0">Nicomoji+</h2>
-          </div>
+        <StickySidebar { ...self.props } font="kokoro" show={ self.state.sidebar }>
+          <SpecimenKokoro { ...self.props } />
         </StickySidebar>
         <div className="flex-none clearfix relative z4">
           <div className="bg-red" style={{ height: 500 + 'px' }}>Footer</div>
@@ -111,6 +173,11 @@ class Index extends React.Component {
       </div>
     )
   }
+}
+
+Index.defaultProps = {
+  // $breakpoint-md
+  sidebarMaxWidth: 800
 }
 
 export default Index
