ab9756a8af3bb1677259eecbf151759f338b0db7
Kenneth Ormandy <kenneth@chloi.io>
2016-09-28 00:39:24 -0700

    Adds minHeight for visualising scroll flow

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-// TODO Remove `.overflow-hidden`
+// TODO Remove `.overflow-hidden` and min height
 class Section extends React.Component {
   render () {
     return (
-      <div className={ this.props.className + ' px' + this.props.basePaddingX + ' py' + this.props.basePaddingY + ' sm-py3 md-py4 overflow-hidden' }>
+      <div className={ this.props.className + ' px' + this.props.basePaddingX + ' py' + this.props.basePaddingY + ' sm-py3 md-py4 overflow-hidden' }
+           style={{ minHeight: 80 + 'vh' }}>
         <div className={ 'max-width-' + this.props.maxWidth + ' mx-auto' }>
           {this.props.children}
         </div>
