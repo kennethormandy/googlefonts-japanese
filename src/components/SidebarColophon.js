0092930f694a794c3fd1f4b06ef5d126afb41e84
Kenneth Ormandy <kenneth@chloi.io>
2016-10-02 14:10:13 -0700

    Adds descriptions from Mariko to the colophon sidebar

--- src/components/SidebarColophon.js
+++ src/components/SidebarColophon.js
@@ -7,9 +7,19 @@ class SidebarColophon extends React.Component {
     const self = this
     var data = self.props.data
     var font = data.fonts[self.props.font]
+    var desc = ''
 
     var fontNameEn = font.name.ja !== font.name.en ? <span lang="en" className="block font-weight-400 muted">{ font.name.en }</span> : <span className="block speak-none">&nbsp;</span>
     var fontDesignerNameEn = font.designer.name.ja !== font.designer.name.en ? <span lang="en" className="muted"> { font.designer.name.en }</span> : ''
+
+
+    if (font.description) {
+      if (font.description.ja && font.description.en) {
+        desc = <div><p className="mt0">{ font.description.ja }</p><p lang="en">{ font.description.en }</p></div>
+      } else {
+        desc = <div><p className="mt0" lang="en">{ font.description.en ? font.description.en : font.description.ja }</p></div>
+      }
+    }
 
     return (
       <StickyContainer style={{ zIndex: 5 }}>
@@ -25,16 +35,19 @@ class SidebarColophon extends React.Component {
                 <div className={ 'h5 md-h4 animate-bg bg-' + self.props.backgroundColor } style={{ height: 100 + 'vh' }}>
                 <div className={ 'p2 md-p3 animate-bg bg-' + (self.props.backgroundColor === 'white' ? 'darken-1' : 'lighten-3') } style={{ height: 100 + 'vh' }}>
                   <h3 className="border-top pt2 inline-block mt0 font-weight-600">{ font.name.ja }{ fontNameEn }</h3>
-                  <div style={{ height: 15 + 'em' }}> {/* So the changing content is less jarring */}
-                  <blockquote>
-                  <p className="lg-h3">{ font.designer.quote.ja }</p>
-                  <p lang="en">{ font.designer.quote.en }</p>
-                  <footer>
-                    <span>{ font.designer.name.ja }</span>
-                    { fontDesignerNameEn }
-                  </footer>
-                  </blockquote>
+
+                  {/* Fixes heights so the changing content is less jarring */}
+                  <div style={{ minHeight: 12 + 'em' }} className="mb3">
+                    <blockquote>
+                      <p className="md-h3 lg-h2">{ font.designer.quote.ja }</p>
+                      <p className="muted-dark" lang="en">{ font.designer.quote.en }</p>
+                      <footer>
+                        <span>{ font.designer.name.ja }</span> <span className="muted-dark" lang="en">{ fontDesignerNameEn }</span>
+                      </footer>
+                    </blockquote>
                   </div>
+                  <div style={{ minHeight: 12 + 'em' }} className="h5 md-h4">{ desc }</div>
+
 
                   <div className="mb2">
                   <abbr className="caps muted border-none">HTML</abbr>
