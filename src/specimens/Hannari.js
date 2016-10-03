b61419f874d38d71aafa1b1cb04cee9b16a81783
Kenneth Ormandy <kenneth@chloi.io>
2016-10-03 10:41:23 -0700

    Improves Hannari section sizing with specific header class

--- src/specimens/Hannari.js
+++ src/specimens/Hannari.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import FitText from 'react-fittext'
 
 class SpecimenHannari extends React.Component {
   render () {
     return (
-      <div className="bg-fallback-red bg-gradient-transparent-white-transparent white">
+      <div className="bg-fallback-red bg-gradient-transparent-white-transparent white overflow-y-hidden">
         <div className="p2 py4">
-        <div className="mx-auto max-width-3">
-          <div className="col-12 h1 wf-hannari">
-            <div className="col-12 md-col-9">
-              <div className="h1 col-12 center mx-auto vertical upright" style={{ minHeight: 80 + 'vh', fontSize: 12 + 'vh' }}>
-                <div>あかるく、</div>
-                <div>やさしく、</div>
-                <div>ふんわり。</div>
-              </div>
+        <div className="mx-auto max-width-4">
+          <div className="col-12">
+            <div className="h0-hannari wf-hannari center mx-auto vertical upright"
+                 style={{ minHeight: 100 + 'vh' }}>
+              <div>あかるく、</div>
+              <div>やさしく、</div>
+              <div>ふんわり。</div>
             </div>
           </div>
         </div>
