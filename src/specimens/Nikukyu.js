b471669ce7d12cef85ea27940fabc65db9ab8f8f
Kenneth Ormandy <kenneth@chloi.io>
2016-10-03 14:40:14 -0700

    Adds spacing back to Nikukyu section

--- src/specimens/Nikukyu.js
+++ src/specimens/Nikukyu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Section from '../components/Section'
+import FitterHappierText from '../vendor/react-fitter-happier-text.jsx'
 
 class SpecimenExample extends React.Component {
   render () {
@@ -8,45 +9,38 @@ class SpecimenExample extends React.Component {
     const font = data.fonts['nikukyu']
 
     return (
-      <Section maxWidth={5} className="bg-fallback-green black wf-nikukyu">
+      <Section maxWidth={5} className="bg-fallback-olive black wf-nikukyu md-px4">
+        <div className="col-12">
 
-      <div className="mx-auto max-width-5">
-          <div className="col-12">
+          <div className="relative justify-center flex h0-nikukyu my4 sm-my0 line-height-1">
+            <span className="block align-bottom"><br/>トン</span>
+            <span className="block align-top">トン</span>
+          </div>
 
-            <div className="relative justify-center green flex h0 my4 sm-my0 line-height-1">
-              <span className="block align-bottom"><br/>トン</span>
-              <span className="block align-top">トン</span>
+          <div className="relative justify-end flex h1 my4 sm-my0 line-height-1">
+            <span className="block align-bottom" style={{ transform: 'rotate(' + 5 + 'deg)' }}><br/>ピョン</span>
+            <span className="block align-top" style={{ transform: 'rotate(' + 5 + 'deg)' }}>ピョン</span>
+          </div>
+
+          <div className="relative my4 sm-my0 h0-nikukyu">
+            <span>モタ　モタ</span>
+          </div>
+
+          <div className="relative max-width-4 mx-auto mb4">
+            <div data-prefix="チョコ チョコ" className="img-nikukyu-ladybug z1 relative">
+            <img src="/media/illustration-nikukyu-ladybug.svg" style={{ width: 35 + 'px' }} />
             </div>
-
-            <div className="relative justify-end flex h1 my4 sm-my0 line-height-1">
-              <span className="block align-bottom" style={{ transform: 'rotate(' + 5 + 'deg)' }}><br/>ピョン</span>
-              <span className="block align-top" style={{ transform: 'rotate(' + 5 + 'deg)' }}>ピョン</span>
-            </div>
-
-            <div className="relative my4 sm-my0 h0">
-              <span>モタ　モタ</span>
-            </div>
-
-            <div className="relative max-width-4 mx-auto mb4">
-              <div data-prefix="チョコ チョコ" className="img-nikukyu-ladybug">
-              <img src="/media/illustration-nikukyu-ladybug.svg" style={{ width: 35 + 'px' }} />
+            <div className="flex items-baseline line-height-1 muted">
+              <div className="col-6 h1">
+                <FitterHappierText text="スタ" paddingY={ 0 } baseline={ 14 } />
               </div>
-              <div className="flex items-baseline line-height-1 green">
-                <div className="col-6">
-                  <div className="js-fitterHappierText">
-                    <span className="h1">スタ</span>
-                  </div>
-                </div>
-                <div className="col-6">
-                  <div className="h0">スタ</div>
-                </div>
+              <div className="col-6">
+                <div className="h0-nikukyu">スタ</div>
               </div>
             </div>
-
           </div>
 
         </div>
-
       </Section>
     )
   }
