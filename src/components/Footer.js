1489dd2ee7a5e8097c122a1310e613d77a868ce7
Kenneth Ormandy <kenneth@chloi.io>
2016-10-03 15:09:12 -0700

    Updates footer content and order

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -47,29 +47,29 @@ class Footer extends React.Component {
               </ul>
             </div>
             <div className="col-8">
-              <p className="mt0">{ data.description }</p>
-              <div className="flex mxn1" lang="en">
-                <div className="col-4 px1">
-                  <a className="border-none" href={ data.authors['marikotakagi'].url }>{ data.authors['marikotakagi'].name }</a>
-                  { data.authors['marikotakagi'].credit }
-                </div>
-                <div className="col-4 px1">
-                  <a className="border-none" href={ data.authors['kennethormandy'].url }>{ data.authors['kennethormandy'].name }</a>
-                  { data.authors['kennethormandy'].credit }
-                </div>
-                <div className="col-4 px1">
-                  <h5 className="h5 mt0">Google Fonts team</h5>
-                  <ul className="list-style-none m0 p0">
-                    <li><a className="border-none" href={ data.authors['davecrossland'].url }>{ data.authors['davecrossland'].name }</a></li>
-                    <li><a className="border-none" href={ data.authors['yuinchien'].url }>{ data.authors['yuinchien'].name }</a></li>
-                    <li><a className="border-none" href={ data.authors['omerziv'].url }>{ data.authors['omerziv'].name }</a></li>
-                    <li><a className="border-none" href={ data.authors['lassefister'].url }>{ data.authors['lassefister'].name }</a></li>
-                  </ul>
-                </div>
+              <div className="clearfix mb4">
+                <h3 className="h3 mt0">Featued Typefaces</h3>
+                <ul className="list-style-none p0 m0">{ typefaceList }</ul>
               </div>
               <div className="clearfix">
-                <h3>Featued Typefaces</h3>
-                <ul className="list-style-none p0 m0">{ typefaceList }</ul>
+                <h3 className="h3">Made by Friends of Google Fonts</h3>
+                <div className="flex mxn1" lang="en">
+                  <div className="col-4 px1">
+                    <a className="" href={ data.authors['marikotakagi'].url }>{ data.authors['marikotakagi'].name }</a>
+                    <p className="mt0">{ data.authors['marikotakagi'].credit }</p>
+                  </div>
+                  <div className="col-4 px1">
+                    <a className="" href={ data.authors['kennethormandy'].url }>{ data.authors['kennethormandy'].name }</a>
+                    <p className="mt0">{ data.authors['kennethormandy'].credit }</p>
+                  </div>
+                  <div className="col-4 px1">
+                    <a href="https://fonts.google.com">Google Fonts</a>
+                    <ul className="list-style-none m0 p0">
+                      <li><a className="border-none" href={ data.authors['yuinchien'].url }>{ data.authors['yuinchien'].name }, art direction</a></li>
+                      <li><a className="border-none" href={ data.authors['lassefister'].url }>{ data.authors['lassefister'].name }, font mastering</a></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
