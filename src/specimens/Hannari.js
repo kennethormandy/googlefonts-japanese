1188783854ac4962fddb1c58eefff84b7d6f79c8
Kenneth Ormandy <kenneth@chloi.io>
2016-10-03 09:18:21 -0700

    Adds background to Hannari section

--- src/specimens/Hannari.js
+++ src/specimens/Hannari.js
@@ -3,7 +3,7 @@ import React from 'react'
 class SpecimenHannari extends React.Component {
   render () {
     return (
-      <div className="bg-fallback-red bg-gradient-red-white white">
+      <div className="bg-fallback-red bg-gradient-transparent-white-transparent white">
         <div className="p2 py4">
         <div className="mx-auto max-width-3">
           <div className="col-12 h1 wf-hannari">
