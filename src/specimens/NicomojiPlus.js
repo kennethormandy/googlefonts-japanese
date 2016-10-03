2bde4b390df05ba69b552b1ae66a33d6d20dd6a8
Kenneth Ormandy <kenneth@chloi.io>
2016-10-03 14:02:15 -0700

    Improves Nicomoji+ section cycling

--- src/specimens/NicomojiPlus.js
+++ src/specimens/NicomojiPlus.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Keyframes, Frame } from 'react-keyframes'
+import map from 'lodash.map'
 import Section from '../components/Section'
 import FitterHappierText from '../vendor/react-fitter-happier-text'
 
@@ -12,18 +14,22 @@ class SpecimenNicomojiPlus extends React.Component {
       <Section
         maxWidth={4}
         basePaddingX={3}
-        className="bg-fallback-black green wf-nicomojiplus">
+        className="bg-fallback-black green wf-nicomojiplus overflow-hidden">
 
-        <img src="/media/illustration-storefront-2-outlined.svg" class="col-12 block" />
-        <div class="flex items-center px1 center height-nicomojiplus">
-          <div class="h0 line-height-1 mt0 mb2 wf-nicomojiplus mx-auto">
-            <FitterHappierText text="ニCもじ" />
-            <FitterHappierText text="カフェでコーヒ" />
-            <FitterHappierText text="おちゃにしますか?" />
-            <FitterHappierText text="いらっしゃいませ!" />
+        <img src="/media/illustration-storefront-2-outlined.svg" className="col-12 block" />
+        <div className="flex items-center px3 my3 center height-nicomojiplus blue">
+          <div className="line-height-1 mt0 mb2 wf-nicomojiplus mx-auto col-10">
+            <Keyframes loop={ true }>
+
+              <Frame duration={ 3000 }><div className="red transition-color"><FitterHappierText text="ニCもじ" /></div></Frame>
+              <Frame duration={ 3000 }><div className="green transition-color"><FitterHappierText text="カフェでコーヒ" /></div></Frame>
+              <Frame duration={ 3000 }><div className="blue transition-color"><FitterHappierText text="おちゃにしますか" /></div></Frame>
+              <Frame duration={ 3000 }><div className="purple transition-color"><FitterHappierText text="いらっしゃいませ" /></div></Frame>
+
+            </Keyframes>
           </div>
         </div>
-        <img src="/media/illustration-storefront-3-outlined.svg" class="col-12 block" />
+        <img src="/media/illustration-storefront-3-outlined.svg" className="col-12 block" />
 
       </Section>
     )
