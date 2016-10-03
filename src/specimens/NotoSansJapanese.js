724c4405022abb28afce07ca89f8cc8504358c7f
Kenneth Ormandy <kenneth@chloi.io>
2016-10-03 15:26:48 -0700

    Adds Noto mountain specimen

--- src/specimens/NotoSansJapanese.js
+++ src/specimens/NotoSansJapanese.js
@@ -4,6 +4,7 @@ import Section from '../components/Section'
 class SpecimenNotoSans extends React.Component {
   render () {
     return (
+    <div className="relative overflow-hidden">
     <Section className="bg-fallback-fuchsia black">
       <div className="wf-notosansjapanese js-bigIdeasText hide">
         <span className="font-weight-900">本当にすばらしい旅だった</span>
@@ -22,6 +23,16 @@ class SpecimenNotoSans extends React.Component {
         <span className="font-weight-200">の人々がいたが、時間が足りなかった。</span>
       </div>
     </Section>
+    <div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z2" style={{ minWidth: 200 + 'vw', minHeight: 200 + 'vw', transform: 'rotate(135deg) translate(50%, 50%)' }}></div>
+    <div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z1" style={{ minWidth: 100 + 'vw', minHeight: 200 + 'vw', transform: 'rotate(136deg) translate(80%, 50%)' }}></div>
+    <div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z2" style={{ minWidth: 300 + 'vw', minHeight: 200 + 'vw', transform: 'rotate(134deg) translate(10%, 50%)' }}></div>
+
+    <div className="animate-float" style={{ animationDuration: 30 + 's', animationDelay: 1 + 's' }}><div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z1" style={{ minWidth: 20 + 'vw', minHeight: 30 + 'vw' }}></div></div>
+    <div className="animate-float" style={{ animationDuration: 60 + 's', animationDelay: 5 + 's' }}><div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z1" style={{ minWidth: 50 + 'vw', minHeight: 50 + 'vw' }}></div></div>
+    <div className="animate-float" style={{ animationDuration: 15 + 's', animationDelay: 3 + 's' }}><div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z1" style={{ minWidth: 70 + 'vw', minHeight: 70 + 'vw' }}></div></div>
+    <div className="animate-float" style={{ animationDuration: 33 + 's', animationDelay: 10 + 's' }}><div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z1" style={{ minWidth: 50 + 'vw', minHeight: 50 + 'vw' }}></div></div>
+    <div className="animate-float" style={{ animationDuration: 25 + 's', animationDelay: 4 + 's' }}><div className="transform-square bg-aqua muted absolute bottom-0 left-0 blend-multiply z1" style={{ minWidth: 40 + 'vw', minHeight: 40 + 'vw' }}></div></div>
+    </div>
     )
   }
 }
