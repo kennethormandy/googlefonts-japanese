64ac1dc9b15b25f1a68ac724f66c9387c4854533
Kenneth Ormandy <kenneth@chloi.io>
2016-10-07 16:13:53 -0700

    Reduces visual emphasis on code blocks in specimen info sidebar

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -5,7 +5,7 @@ class CodeBlock extends React.Component {
     const self = this
 
     return (
-      <pre className="overflow-x-auto h5 line-height-4 bg-black white rounded p2 font-family-monospace"
+      <pre className="overflow-x-auto h5 md-h4 line-height-4 bg-lighten-2 black rounded p2 font-family-monospace"
            data-language={ self.props.language }><code>{ self.props.children.toString() }</code></pre>
     )
   }
