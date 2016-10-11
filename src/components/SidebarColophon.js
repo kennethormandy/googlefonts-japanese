2c44efb29e236ad8ec288802b3586ca0e2dbab1d
Kenneth Ormandy <kenneth@chloi.io>
2016-10-11 01:17:01 -0700

    Updates desc conditional

--- src/components/SidebarColophon.js
+++ src/components/SidebarColophon.js
@@ -21,12 +21,16 @@ class SidebarColophon extends React.Component {
     }
 
     // TODO switch to toggle
-    if (font.description) {
-      if (font.description.ja && font.description.en) {
-        desc = <div><p className="mt0">{ font.description.ja }</p><p lang="en">{ font.description.en }</p></div>
-      } else {
-        desc = <div><p className="mt0" lang="en">{ font.description.en ? font.description.en : font.description.ja }</p></div>
-      }
+    // There are no translated versions of the descriptions right now
+    // if (font.description) {
+    //   if (font.description.ja && font.description.en) {
+    //     desc = <div><p className="mt0">{ font.description.ja }</p><p lang="en">{ font.description.en }</p></div>
+    //   } else {
+    //     desc = <div><p className="mt0" lang="en">{ font.description.en ? font.description.en : font.description.ja }</p></div>
+    //   }
+    // }
+    if (font.description && font.description.en) {
+      desc = <p className="m0" lang="en">{ font.description.en }</p>
     }
 
     return (
@@ -50,6 +54,9 @@ class SidebarColophon extends React.Component {
                   <h3 className="border-top pt2 onum pnum inline-block mt0 font-weight-600">{ font.name.ja }{ fontNameEn }</h3>
 
                   {/* Fixes heights so the changing content is less jarring */}
+                  {/* TODO might need to change this minHeight based on CSS breakpoints */}
+                  <div style={{ minHeight: 12 + 'em' }} className="h5 md-h4">{ desc }</div>
+
                   <div style={{ minHeight: 12 + 'em' }} className="mb3">
                     <blockquote>
                       <p className="md-h3 lg-h2">{ font.designer.quote.ja }</p>
@@ -59,8 +66,6 @@ class SidebarColophon extends React.Component {
                       </footer>
                     </blockquote>
                   </div>
-                  <div style={{ minHeight: 12 + 'em' }} className="h5 md-h4">{ desc }</div>
-
 
                   <div className="mb2">
                   <abbr className="muted border-none">HTML</abbr>
