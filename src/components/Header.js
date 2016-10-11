a6a778d344fabb1dd0fe931ff96f4b94838d1510
Kenneth Ormandy <kenneth@chloi.io>
2016-10-11 09:41:08 -0700

    Adds some font weight variation to header nav, fixes lang attribute

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,18 +28,20 @@ class Header extends React.Component {
 
     var typefaceList = _map(Object.keys(data.fonts), function (index) {
       var font = data.fonts[index]
+      var fontWeight = (index === 'mplus1p' || index === 'roundedmplus1c' || index === 'notosansjapanese') ? 800 : 400
+
       if (font.published !== false && font.designer) {
         return (
-          <li className="col-12 sm-col-6 md-col-4 px2 m0" key={ 'footer_' + index }><a className="block py2 border-none" href={ '#' + index }>
+          <li className="col-12 sm-col-6 md-col-4 px2 m0" key={ 'header_' + index }><a className="block py2 border-none" href={ '#' + index }>
             <div className="flex items-center">
-              <div className="h1 col-4 sm-col-3 md-col-6 right-align pr2 maroon">
+              <div className="h1 col-4 sm-col-3 md-col-5 center maroon">
                 <FitText compressor={0.33} minFontSize={24}>
-                  <div className={ 'break-none wf-' + index }>あア</div>
+                  <div className={ 'break-none wf-' + index + ' font-weight-' + fontWeight }>あア</div>
                 </FitText>
               </div>
-              <div className="flex-auto">
-                <span>{ font.name.ja } <span lang="en" className="muted">{ font.name.en !== font.name.ja ? font.name.en : '' }</span></span>
-                <span className="block mt1">{ font.designer.name.ja } <span lang="muted" className="muted">{ font.designer.name.en !== font.designer.name.ja ? font.designer.name.en : '' }</span></span>
+              <div className="flex-auto line-height-2 pl2">
+                <span>{ font.name.ja } <span lang="en" className="muted-dark">{ font.name.en !== font.name.ja ? font.name.en : '' }</span></span>
+                <span className="block mt1">{ font.designer.name.ja } <span lang="en" className="muted-dark">{ font.designer.name.en !== font.designer.name.ja ? font.designer.name.en : '' }</span></span>
               </div>
             </div>
           </a></li>
