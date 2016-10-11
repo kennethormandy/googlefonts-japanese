bedcdf12862ab3ce1b88f73ca4494c568984a123
Kenneth Ormandy <kenneth@chloi.io>
2016-10-11 09:41:46 -0700

    Updates test page

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SidebarColophon from '../components/SidebarColophon'
-import SpecimenSawarabi from '../specimens/Sawarabi'
+import TestSpecimen from '../specimens/Sawarabi'
 
 class Test extends React.Component {
   constructor () {
@@ -15,10 +15,15 @@ class Test extends React.Component {
     const data = self.props.data
 
     return (
+      <div>
+      <div style={{ height: 33 + 'vh' }} className="h1 bg-blue">Hello</div>
+      <div style={{ height: 33 + 'vh' }} className="h1 bg-red">Hello</div>
+      <div style={{ height: 33 + 'vh' }} className="h1 bg-green">Hello</div>
       <div className="clearfix" className={ 'bg-' + self.state.backgroundColor }>
         <SidebarColophon { ...self.props } font={ 'sawarabi' } show={ true } backgroundColor={ 'transparent' }>
-          <SpecimenSawarabi { ...self.props } />
+          <TestSpecimen { ...self.props } />
         </SidebarColophon>
+      </div>
       </div>
     )
   }
