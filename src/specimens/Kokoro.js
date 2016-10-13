f61e1a7192412fda7c0f540e4cfdd97a6721ab87
Kenneth Ormandy <kenneth@chloi.io>
2016-10-12 23:22:51 -0700

    Improves Kokoro book page size across breakpoints

--- src/specimens/Kokoro.js
+++ src/specimens/Kokoro.js
@@ -9,16 +9,19 @@ class KokoroNotebook extends React.Component {
 
     return (
       <div className="height-kokoro-book mx-auto max-width-4 bg-white black ds-bookcover rounded flex flex-wrap items-stretch content-stretch overflow-hidden">
-        <div className="height-kokoro-page col-12 md-col-6 ds-page-left sm-rounded-left flex items-center">
-          <div className="flex-auto p3" style={{ transform: 'translateY(-1rem) rotate(3deg)' }}>
-            <div className="fill-red muted blend-multiply mx-auto max-width-1">
+        <div className="height-kokoro-page col-12 sm-col-6 md-col-12 lg-col-6 ds-page-left sm-rounded-left flex items-center">
+          <div className="flex-auto p3">
+            <div className="fill-red muted-dark blend-multiply md-hide lg-hide" aria-label="Secret mark" style={{ maxWidth: 3 + 'rem' }}>
               <KokoroSecret />
             </div>
           </div>
         </div>
-        <div className="height-kokoro-page col-12 md-col-6 ds-page-right flex items-center">
-          <div className="relative p3">
-            <div className="max-width-2 mx-auto h4 md-h2 lg-h1 line-height-3">{ self.props.children }</div>
+        <div className="height-kokoro-page col-12 sm-col-6 md-col-12 lg-col-6 ds-page-right flex items-center">
+          <div className="relative p3 flex flex-wrap">
+            <div className="self-start fill-red muted-dark xs-hide sm-hide border" style={{ height: 50 + '%', maxWidth: 3 + 'rem' }}>
+              <KokoroSecret />
+            </div>
+            <div className="self-baseline max-width-2 mx-auto h5 md-h4 border lg-h1 line-height-4">{ self.props.children }</div>
           </div>
         </div>
       </div>
