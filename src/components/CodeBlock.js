f2162b665e8650424a87905f8587ef8a74c27797
Kenneth Ormandy <kenneth@chloi.io>
2016-10-13 12:26:03 -0700

    Fixes vertical alignment for code block in sidebar, thanks Yuin

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -3,7 +3,7 @@ import React from 'react'
 class CodeBlock extends React.Component {
   render () {
     const self = this
-    var className = 'overflow-x-auto h5 md-h4 line-height-3 bg-lighten-2 black p2 font-family-monospace'
+    var className = 'overflow-x-auto h5 md-h4 line-height-3 bg-lighten-2 black p2 md-px3 font-family-monospace'
 
     if (self.props.rounded) {
       className += ' rounded'
