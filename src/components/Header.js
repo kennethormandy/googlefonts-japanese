2fdb205621d4abeeddc30448e7f8c2d4b206cbd0
Kenneth Ormandy <kenneth@chloi.io>
2016-10-12 17:25:17 -0700

    Updates header max-width, Noto Sans

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,14 +30,16 @@ class Header extends React.Component {
     return (
       <div className="bg-white black font-family-base relative pb4 bg-white black" style={{ zIndex: 100 }}>
         <header className="mx-auto md-max-width-4 lg-max-width-5 mb4 px1 md-px2 lg-px3 pt2 md-pt3 font-weight-500">
-          <div className="h4 md-h3 font-family-product-sans red" lang="en">Google Fonts + Japanese Early Access</div>
-          <FitText compressor={ 0.95 }>
-          <h1 className="h1 line-height-2 my0 mb3 red">
-            <span className="font-family-product-sans">Google Fonts</span>
-            <span style={{ fontSize: 0.9 + 'em' }} className="font-family-600">&nbsp;+&nbsp;日本語 <span>早期アクセス</span></span>
-          </h1>
-          </FitText>
-          <div className="sm-flex sm-mxn1 md-mxn2 line-height-4">
+          <div className="h4 md-h3 red" lang="en">Google Fonts + Japanese Early Access</div>
+          <div className="col-12 md-col-9">
+            <FitText compressor={ 1 }>
+              <h1 className="h1 line-height-2 my0 mb3 red font-weight-500">
+                <span className="break-none">Google Fonts</span><span style={{ fontSize: 0.9 + 'em' }}>&nbsp;+&nbsp;日本語</span><br/>
+                <span style={{ fontSize: 0.9 + 'em' }}>早期アクセス</span>
+              </h1>
+            </FitText>
+          </div>
+          <div className="sm-flex sm-mxn1 md-mxn2 line-height-4 font-weight-400">
             <HeaderColumn>
               <p>Google Fonts は、デザイナー ウェブフォントを直感的にご利用いただける、安定したオープンソース ディレクトリです。Google Fonts 早期アクセスでは、現時点で完全にはサポートしていないフォントを試験的に提供しています。</p>
               <p>膨大な数の文字のデザインに要する時間や、大容量のフォント ファイルをウェブフォントとして提供する際に必要な技術的インフラストラクチャなど、日本語の書体デザインやフォント開発にはさまざまな課題が立ちふさがります。そこで、規格や技術の開発を進めると同時に、Google Fonts 早期アクセスを通じてデザイナーやデベロッパーの皆様に日本語のウェブフォントをお試しいただけるようにしました。</p>
