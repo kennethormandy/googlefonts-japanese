114ae01cf2e5cf54ff810615c12b377c90ae0a72
SnO₂WMaN <numaguchisow@gmail.com>
2016-11-10 02:16:05 +0900

    Fix #10 and #7 (#12) by SnO2WMaN
    * Fix Typo Mistake 1
    
    Fix ニCもじ to ニコもじ
    
    * Fix Typo Mistake 2
    
    Fix カフェでコーヒ to カフェでコーヒー

--- src/specimens/NicoMoji.js
+++ src/specimens/NicoMoji.js
@@ -16,8 +16,8 @@ class SpecimenNicoMoji extends React.Component {
           <div className="line-height-1 mt0 mb2 wf-nicomoji mx-auto col-10">
             <Keyframes loop={true}>
 
-              <Frame duration={2500}><div className="red transition-color"><FitterHappierText text="ニCもじ" /></div></Frame>
-              <Frame duration={2500}><div className="green transition-color"><FitterHappierText text="カフェでコーヒ" /></div></Frame>
+              <Frame duration={2500}><div className="red transition-color"><FitterHappierText text="ニコもじ" /></div></Frame>
+              <Frame duration={2500}><div className="green transition-color"><FitterHappierText text="カフェでコーヒー" /></div></Frame>
               <Frame duration={2500}><div className="blue transition-color"><FitterHappierText text="おちゃにしますか" /></div></Frame>
               <Frame duration={2500}><div className="purple transition-color"><FitterHappierText text="いらっしゃいませ" /></div></Frame>
 
