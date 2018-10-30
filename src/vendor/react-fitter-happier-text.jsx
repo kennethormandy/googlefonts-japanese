030bba62fd4ab5383ab700a1da8737abe1642df7
kennethormandy <hello@kennethormandy.com>
2018-10-29 20:45:06 -0700

    Updates React Fitter Happier Text in vendor to avoid using old React text refs

--- src/vendor/react-fitter-happier-text.jsx
+++ src/vendor/react-fitter-happier-text.jsx
@@ -1,7 +1,9 @@
+// @kennethormandy/react-fitter-happier-text v2.0.0-rc.2
+// MIT via https://github.com/jxnblk/react-fitter-happier-text
 
 import React from 'react'
-import debounce from 'lodash.debounce'
 import PropTypes from 'prop-types'
+import debounce from 'lodash.debounce'
 
 class FitterHappierText extends React.Component {
 
@@ -15,15 +17,17 @@ class FitterHappierText extends React.Component {
   }
 
   resize () {
-    let el = this.refs.text;
-    let state = this.state
-    let width = el.offsetWidth || el.getComputedTextLength()
-    let height = el.offsetHeight || 24
-    if (state.width !== width || state.height !== height) {
-      this.setState({
-        width: width,
-        height: height
-      })
+    if (typeof this.el !== 'undefined') {
+      let el = this.el;
+      let state = this.state
+      let width = el.offsetWidth || el.getComputedTextLength()
+      let height = el.offsetHeight || 24
+      if (state.width !== width || state.height !== height) {
+        this.setState({
+          width: width,
+          height: height
+        })
+      }
     }
   }
 
@@ -34,8 +38,14 @@ class FitterHappierText extends React.Component {
   componentWillReceiveProps () {
     this.resize()
   }
+  
+  componentWillUnmount () {
+    this.resize = null
+  }
 
   render () {
+    return this.props.text
+
     let styles = {
       svg: {
         width: '100%',
@@ -61,7 +71,7 @@ class FitterHappierText extends React.Component {
         viewBox={viewBox}
         style={styles.svg}>
         <text
-          ref='text'
+          ref={(el) => { this.el = el }}
           x='50%'
           y={this.props.baseline}
           style={styles.text}>
