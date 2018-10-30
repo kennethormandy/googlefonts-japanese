6e4e638403c27b7a1cc33ed177aeee1565eaf335
kennethormandy <hello@kennethormandy.com>
2018-10-29 20:46:42 -0700

    Fixes React v16 + Google Translate issue

--- src/router.js
+++ src/router.js
@@ -10,7 +10,47 @@ export default Router.extend({
     if (opts.layout) {
       page = <Layout>{page}</Layout>;
     }
-    render(page, document.getElementById('js-root'));
+
+    // https://github.com/facebook/react/issues/11538#issuecomment-417504600
+    /*eslint-disable */
+    if (typeof Node === "function" && Node.prototype) {
+      const originalRemoveChild = Node.prototype.removeChild;
+      Node.prototype.removeChild = function(child) {
+        if (child.parentNode !== this) {
+          // eslint-ignore
+          if (console) {
+            console.error(
+              // eslint-ignore
+              "Cannot remove a child from a different parent",
+              child,
+              this
+            );
+          }
+          return child;
+        }
+        return originalRemoveChild.apply(this, arguments);
+      };
+
+      const originalInsertBefore = Node.prototype.insertBefore;
+      Node.prototype.insertBefore = function(newNode, referenceNode) {
+        if (referenceNode && referenceNode.parentNode !== this) {
+          // eslint-ignore
+          if (console) {
+            // eslint-ignore
+            console.error(
+              "Cannot insert before a reference node from a different parent",
+              referenceNode,
+              this
+            );
+          }
+          return newNode;
+        }
+        return originalInsertBefore.apply(this, arguments);
+      };
+    }
+    /*eslint eqeqeq:0*/
+
+    render(page, document.getElementById("js-root"));
   },
   routes: {
     "": "home",
