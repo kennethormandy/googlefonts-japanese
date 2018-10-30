812d9cea4c906608f97d27f0387df34b9cd0cd4b
kennethormandy <hello@kennethormandy.com>
2018-10-29 23:45:16 -0700

    Fixes linting

--- src/components/EarlyAccessFlag.js
+++ src/components/EarlyAccessFlag.js
@@ -5,7 +5,7 @@ const EarlyAccessFlag = (props) => {
   return (
     <div
       className="h6 lg-h5 absolute border-top line-height-3 col-1 top-0 right-0 border-gray gray pt2 font-weight-400"
-      style={{minHeight: '100px', height: '100%', marginTop: '-1px', marginRight: '-0.75rem' }}>
+      style={{minHeight: '100px', height: '100%', marginTop: '-1px', marginRight: '-0.75rem'}}>
       <div className="vertical">
         <div lang="en">{props.label.en}</div>{' '}
         <div lang="ja" style={{fontSize: '0.9em'}}>
